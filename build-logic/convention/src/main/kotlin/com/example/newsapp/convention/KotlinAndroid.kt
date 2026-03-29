1aa48e683c3e935ab82f4a7f884e8f0b8ca6d2f0
alugovskoy <alexlugoff@gmail.com>
2026-03-29 16:59:31 +0300

    update dependencies

--- build-logic/convention/src/main/kotlin/com/example/newsapp/convention/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/example/newsapp/convention/KotlinAndroid.kt
@@ -4,6 +4,7 @@ import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.assign
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
@@ -49,7 +50,7 @@ internal fun Project.configureKotlinAndroid(
 private fun Project.configureKotlin() {
     tasks.withType<KotlinCompile>().configureEach {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_17)
+            jvmTarget = JvmTarget.JVM_17
         }
     }
 }
